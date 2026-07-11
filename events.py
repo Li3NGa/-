42e46b88d2b238733870948659d5c2eb0703b729
Li3NGa <1753864700@qq.com>
2026-07-11 22:22:35 +0800

    Implement realtime chat socket events

--- events.py
+++ events.py
@@ -1,7 +1,68 @@
-from flask_socketio import send
+from flask_socketio import emit, join_room, leave_room
+
+from services.user_service import UserService
+from services.room_service import RoomService
+from services.chat_service import ChatService
+
+
+user_service = UserService()
+room_service = RoomService()
+chat_service = ChatService()
+
+users = {}
 
 
 def register_socket_events(socketio):
-    @socketio.on('message')
-    def handle_message(message):
-        send(message, broadcast=True)
+
+    @socketio.on('connect')
+    def handle_connect():
+        username = user_service.create_anonymous_user()
+        users[username] = username
+
+        emit('user_info', {
+            'username': username
+        })
+
+    @socketio.on('join_room')
+    def handle_join(data):
+        username = data.get('username')
+        room_id = data.get('room_id', 'default')
+
+        join_room(room_id)
+        room_service.join_room(room_id, username)
+
+        emit('system_message', {
+            'message': f'{username} joined room {room_id}'
+        }, room=room_id)
+
+    @socketio.on('leave_room')
+    def handle_leave(data):
+        username = data.get('username')
+        room_id = data.get('room_id', 'default')
+
+        leave_room(room_id)
+        room_service.leave_room(room_id, username)
+
+        emit('system_message', {
+            'message': f'{username} left room {room_id}'
+        }, room=room_id)
+
+    @socketio.on('send_message')
+    def handle_message(data):
+        username = data.get('username')
+        room_id = data.get('room_id', 'default')
+        content = data.get('content')
+
+        message = chat_service.add_message(
+            username,
+            content
+        )
+
+        emit('new_message', message, room=room_id)
+
+    @socketio.on('get_online_users')
+    def online_users(data):
+        room_id = data.get('room_id', 'default')
+        emit('online_count', {
+            'count': room_service.online_count(room_id)
+        })
