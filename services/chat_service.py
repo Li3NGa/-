b28724653ad2d2bb803cf09ec9ce5b06265aa9b4
Li3NGa <1753864700@qq.com>
2026-07-11 22:16:24 +0800

    Upgrade chat service message handling

--- services/chat_service.py
+++ services/chat_service.py
@@ -1,3 +1,18 @@
+from datetime import datetime
+
+
 class ChatService:
-    def broadcast_message(self, message):
+    def __init__(self):
+        self.messages = []
+
+    def add_message(self, username, content):
+        message = {
+            'username': username,
+            'content': content,
+            'time': datetime.now().isoformat()
+        }
+        self.messages.append(message)
         return message
+
+    def get_messages(self):
+        return self.messages
