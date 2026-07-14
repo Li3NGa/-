944f38bd967b0581f97eb6fd5030ca133323eeb2
Li3NGa <1753864700@qq.com>
2026-07-14 10:44:08 +0800

    feat: add room lobby route

--- routes.py
+++ routes.py
@@ -4,4 +4,8 @@ from flask import render_template
 def register_routes(app):
     @app.route('/')
     def index():
+        return render_template('rooms.html')
+
+    @app.route('/chat')
+    def chat():
         return render_template('index.html')
