b70ea43d63cba9035824fab2304aa6565b88f12a
Li3NGa <1753864700@qq.com>
2026-07-14 10:50:10 +0800

    feat: bind messages with room id

--- models/message.py
+++ models/message.py
@@ -4,6 +4,15 @@ from datetime import datetime
 
 @dataclass
 class Message:
+    room_id: int
     username: str
     content: str
     created_at: datetime = datetime.now()
+
+    def to_dict(self):
+        return {
+            'room_id': self.room_id,
+            'username': self.username,
+            'content': self.content,
+            'time': self.created_at.isoformat()
+        }
