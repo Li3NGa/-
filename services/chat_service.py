c06f5c56c2aaad80b0032a60c5f2e0dca7ac3552
Li3NGa <1753864700@qq.com>
2026-07-14 10:50:15 +0800

    feat: isolate chat messages by room

--- services/chat_service.py
+++ services/chat_service.py
@@ -3,16 +3,21 @@ from datetime import datetime
 
 class ChatService:
     def __init__(self):
-        self.messages = []
+        self.messages = {}
 
-    def add_message(self, username, content):
+    def add_message(self, room_id, username, content):
         message = {
+            'room_id': room_id,
             'username': username,
             'content': content,
             'time': datetime.now().isoformat()
         }
-        self.messages.append(message)
+
+        if room_id not in self.messages:
+            self.messages[room_id] = []
+
+        self.messages[room_id].append(message)
         return message
 
-    def get_messages(self):
-        return self.messages
+    def get_messages(self, room_id, limit=100):
+        return self.messages.get(room_id, [])[-limit:]
