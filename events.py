ac09331a8204be9d312336fc8c369de84c1bc366
Li3NGa <1753864700@qq.com>
2026-07-14 10:42:51 +0800

    feat: support multiple independent chat rooms

--- events.py
+++ events.py
@@ -4,7 +4,6 @@ from services.user_service import UserService
 from services.room_service import RoomService
 from services.chat_service import ChatService
 
-
 user_service = UserService()
 room_service = RoomService()
 chat_service = ChatService()
@@ -17,52 +16,48 @@ def register_socket_events(socketio):
     @socketio.on('connect')
     def handle_connect():
         username = user_service.create_anonymous_user()
-        users[username] = username
-
-        emit('user_info', {
-            'username': username
-        })
+        users[username] = {'room': None}
+        emit('user_info', {'username': username})
 
     @socketio.on('join_room')
     def handle_join(data):
         username = data.get('username')
-        room_id = data.get('room_id', 'default')
+        room_id = data.get('room_id', 'public')
 
         join_room(room_id)
+        users.setdefault(username, {})['room'] = room_id
         room_service.join_room(room_id, username)
 
         emit('system_message', {
-            'message': f'{username} joined room {room_id}'
+            'message': f'{username} joined {room_id}'
         }, room=room_id)
+
+        emit('room_joined', {'room_id': room_id})
 
     @socketio.on('leave_room')
     def handle_leave(data):
         username = data.get('username')
-        room_id = data.get('room_id', 'default')
+        room_id = data.get('room_id', 'public')
 
         leave_room(room_id)
         room_service.leave_room(room_id, username)
 
         emit('system_message', {
-            'message': f'{username} left room {room_id}'
+            'message': f'{username} left {room_id}'
         }, room=room_id)
 
     @socketio.on('send_message')
     def handle_message(data):
         username = data.get('username')
-        room_id = data.get('room_id', 'default')
+        room_id = data.get('room_id', 'public')
         content = data.get('content')
 
-        message = chat_service.add_message(
-            username,
-            content
-        )
-
+        message = chat_service.add_message(username, content)
         emit('new_message', message, room=room_id)
 
     @socketio.on('get_online_users')
     def online_users(data):
-        room_id = data.get('room_id', 'default')
+        room_id = data.get('room_id', 'public')
         emit('online_count', {
             'count': room_service.online_count(room_id)
         })
